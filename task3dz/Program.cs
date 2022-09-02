2846f091ed3188d08fe324a74a1714d83dd26723
tegeran87 <mtv-rd@mail.ru>
2022-09-02 19:43:58 +0300

    ДЗ 2

--- task3dz/Program.cs
+++ task3dz/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if(numberA%2==0)
+{
+    Console.WriteLine("да");
+} 
+else
+{
+    Console.WriteLine("нет");
+}
